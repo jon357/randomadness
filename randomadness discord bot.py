dd1e99345f3523c0f9e042056cc6c0f22daff5cf
jon357 <checajonathan@gmail.com>
2017-11-25 13:08:40 +0100

    Update randomadness discord bot.py

--- randomadness discord bot.py
+++ randomadness discord bot.py
@@ -4,9 +4,9 @@ from random import choice
 client = discord.Client()
 LP=[]
 j=0
-clases=["Assaut du mage de bataille","Assault du Coisé","Subterfuge de l'archère","Furie du guerrier","Tromperie du mage","Rituel de l'assasin","Négation du sorcier","Frappe du moine","Invocation de Magisfer","Instincts d'éclaireur"]
-pseudodiscord=['jon357892','Santosvella','ArchFrost','Glandeuranon','ptitjoueur']
-pseudoig=['Jonathan357','Santosvella','ArchFrost','glandeuranon','Ptitjoueur']
+clases=["Assaut du mage de bataille","Assaut du Croisé","Subterfuge de l'archère","Furie du guerrier","Tromperie du mage","Rituel de l'assasin","Négation du sorcier","Frappe du moine","Invocation de Magisfer","Instincts d'éclaireur"]
+pseudodiscord=['jon357892','Santosvella','ArchFrost','Glandeuranon','ptitjoueur','K13b3r','poulpix69']
+pseudoig=['Jonathan357','Santosvella','ArchFrost','glandeuranon','Ptitjoueur','K13b3r','poulpix69']
 modo=['Santosvella','ArchFrost','jon357892']
 
 @client.event
@@ -20,6 +20,7 @@ async def on_message(message):
 			if username==modo[i]:
 				modoac=+1
 		if modoac>=1:
+			print('randomadness lancé')
 			global j
 			if j==0:
 				rl='Randomadness lancé taper !join pour rejoindre la liste d attente'
@@ -39,10 +40,11 @@ async def on_message(message):
 				LP.append(username)
 				print(username,' a rejoin la liste d attente')
 	if message.content.startswith('!liste'):
-		aliste='liste des joueurs : {}'
+		aliste='Liste des joueurs : {}'
 		await client.send_message(discord.Object(id='383618886806929419'),aliste.format(LP))
 	if message.content.startswith('!unjoin'):
 		if j>=1:
+			username=message.author.name
 			LP.remove(username)
 			print(username,' a quiter la liste d attente')
 	if message.content.startswith('!duel'):
@@ -52,8 +54,9 @@ async def on_message(message):
 				NLP=NLP+1
 			if NLP<=1:
 				mp='Il faut plus de joueurs'
-				await client.send_message(message.channel,mp.format())
+				await client.send_message(discord.Object(id='383618886806929419'),mp.format())
 			else:
+				print('duel lancer')
 				R1=choice(LP)
 				J1=R1
 				LP.remove(R1)
@@ -62,52 +65,40 @@ async def on_message(message):
 				LP.remove(R2)
 				C1=choice(clases)
 				C2=choice(clases)
-				for i in range(len(pseudodiscord)):
-					if J1==pseudodiscord[i]:
-						ig=i
-						pig1=1
-				if pig1==1:
-					pseudig1=pseudoig[ig]
+				pig1=0
+				pig2=0
+				for i1 in range(len(pseudodiscord)):
+					if J1==pseudodiscord[i1]:
+						ig1=i1
+						pig1=pig1+1
+				if pig1>=1:
+					pseudig1=pseudoig[ig1]
 				else:
 					pseudig1='non précisé'
-				for i in range(len(pseudodiscord)):
-					if J2==pseudodiscord[i]:
-						ig=i
-						pig2=1
-				if pig2==1:
-					pseudig2=pseudoig[ig]
+				for i2 in range(len(pseudodiscord)):
+					if J2==pseudodiscord[i2]:
+						ig2=i2
+						pig2=pig2+1
+				if pig2>=1:
+					pseudig2=pseudoig[ig2]
 				else:
-					pseudig2='"non précisé'
-				dnp1='{} [ ingame: {} ] - {}' 
-				dnp2='{} [ ingame: {} ] - {}'
-				await client.send_message(discord.Object(id='383618886806929419'),dnp1.format(J1,pseudig1,C1))
-				await client.send_message(discord.Object(id='383618886806929419'),'---------------------------VS---------------------------')
-				await client.send_message(discord.Object(id='383618886806929419'),dnp1.format(J2,pseudig2,C2))
+					pseudig2='non précisé'
+				dnp='{} [ ingame: {} ] - {} \n---------------------------VS--------------------------- \n{} [ ingame: {} ] - {}'
+				await client.send_message(discord.Object(id='383618886806929419'),dnp.format(J1,pseudig1,C1,J2,pseudig2,C2))
 	if message.content.startswith('!stop'):
-			modoac=0
-			username=message.author.name
-			for i in range(len(modo)):
-				if username==modo[i]:
-					modoac=+1
-			if modoac>=1:
-				if j>=1:
-					del LP[:]
-					j=-1
+		modoac=0
+		username=message.author.name
+		for i in range(len(modo)):
+			if username==modo[i]:
+				modoac=+1
+		if modoac>=1:
+			if j>=1:
+				print('randomadness stoper')
+				del LP[:]
+				j=-1
 	if message.content.startswith('!commande'):
-		cmd='voila la liste des commande :'
-		cmdrando='!randomadness pour lancé le jeu , '
-		cmdj='!join pour vous metre en liste d attente ,'
-		cmdl='!liste pour afficher la liste des participent , '
-		cmduj='!unjoin pour vous retirer de la liste d attente , '
-		cmdd='!duel pour tirer au sort un match ,'
-		cmds='!stop pour arreter le jeu .'
+		cmd='voici la liste des commandes : \n!randomadness pour lancer le jeu (modo seulement) , \n!join pour vous mettre en liste d attente , \n!liste pour afficher la liste des participants , \n!unjoin pour vous retirer de la liste d attente , \n!duel pour tirer au sort un match , \n!stop pour arreter le jeu (modo seulement) ,\n!commande pour afficher la liste des commandes .'
 		await client.send_message(discord.Object(id='383618886806929419'),cmd.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmdrando.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmdj.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmdl.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmduj.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmdd.format())
-		await client.send_message(discord.Object(id='383618886806929419'),cmds.format())
 
 @client.event
 async def on_ready():
@@ -117,6 +108,6 @@ async def on_ready():
 	print('------')
 
 #client.run('MzgwMTMwMTYyMjI3OTM3Mjgy.DO0Idg.TTH3LMVssf9BC2mncXXOZcXeusc')	#jon test bot
-#client.run('MzgyOTc0NDI1MDA5NDg3ODgz.DPdgSQ.owv2T2CX-xHuip0MhDjUI7R6WZQ')	#test bot priver
-client.run('MzgzNjE5MjIzNzU2MjEwMTc4.DPm45Q.Oy-nKyLUIurYn-wkMZnezBoaeb8')	#santosvallée
+client.run('MzgyOTc0NDI1MDA5NDg3ODgz.DPdgSQ.owv2T2CX-xHuip0MhDjUI7R6WZQ')	#test bot priver
+#client.run('MzgzNjE5MjIzNzU2MjEwMTc4.DPm45Q.Oy-nKyLUIurYn-wkMZnezBoaeb8')	#santosvallée
 #discord.Object(id='383618886806929419')
