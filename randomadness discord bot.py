edb0f748c7ed01ac087c50fda677c0972cd8ac65
jon357 <checajonathan@gmail.com>
2017-12-01 00:19:08 +0100

    Update randomadness discord bot.py

--- randomadness discord bot.py
+++ randomadness discord bot.py
@@ -5,9 +5,9 @@ client = discord.Client()
 LP=[]
 j=0
 clases=["Assaut du mage de bataille","Assaut du Croisé","Subterfuge de l'archère","Furie du guerrier","Tromperie du mage","Rituel de l'assasin","Négation du sorcier","Frappe du moine","Invocation de Magisfer","Instincts d'éclaireur"]
-pseudodiscord=['jon357892','Santosvella','ArchFrost','Glandeuranon','ptitjoueur','K13b3r','poulpix69']
-pseudoig=['Jonathan357','Santosvella','ArchFrost','glandeuranon','Ptitjoueur','K13b3r','poulpix69']
-modo=['Santosvella','ArchFrost','jon357892']
+pseudodiscord=['jon357892','Santosvella','ArchFrost','Glandeuranon','ptitjoueur','K13b3r','poulpix69','Nexos']
+pseudoig=['Jonathan357','Santosvella','ArchFrost','glandeuranon','Ptitjoueur','K13b3r','poulpix69','Nexos999']
+modo=['Santosvella','ArchFrost','jon357892','Nexos']
 
 @client.event
 async def on_message(message):
@@ -23,11 +23,11 @@ async def on_message(message):
 			print('randomadness lancé')
 			global j
 			if j==0:
-				rl='Randomadness lancé taper !join pour rejoindre la liste d attente'
+				rl="Randomadness lancé taper !join pour rejoindre la liste d attente"
 				await client.send_message(discord.Object(id='383618886806929419'),rl.format())
 				j=+1
 			else:
-				rl='Randomadness deja lancé tapez !join pour jouer'
+				rl="Randomadness deja lancé tapez !join pour jouer"
 				await client.send_message(discord.Object(id='383618886806929419'),rl.format())
 	if message.content.startswith('!join'):
 		uname=0
@@ -40,7 +40,7 @@ async def on_message(message):
 				LP.append(username)
 				print(username,' a rejoin la liste d attente')
 	if message.content.startswith('!liste'):
-		aliste='Liste des joueurs : {}'
+		aliste="Liste des joueurs : {}"
 		await client.send_message(discord.Object(id='383618886806929419'),aliste.format(LP))
 	if message.content.startswith('!unjoin'):
 		if j>=1:
@@ -53,7 +53,7 @@ async def on_message(message):
 			for i in range(len(LP)):
 				NLP=NLP+1
 			if NLP<=1:
-				mp='Il faut plus de joueurs'
+				mp="Il faut plus de joueurs"
 				await client.send_message(discord.Object(id='383618886806929419'),mp.format())
 			else:
 				print('duel lancer')
@@ -83,7 +83,7 @@ async def on_message(message):
 					pseudig2=pseudoig[ig2]
 				else:
 					pseudig2='non précisé'
-				dnp='{} [ ingame: {} ] - {} \n---------------------------VS--------------------------- \n{} [ ingame: {} ] - {}'
+				dnp="{} [ingame:{}] - {} \n---------------------------VS--------------------------- \n{} [ingame:{}] - {}"
 				await client.send_message(discord.Object(id='383618886806929419'),dnp.format(J1,pseudig1,C1,J2,pseudig2,C2))
 	if message.content.startswith('!stop'):
 		modoac=0
@@ -96,8 +96,8 @@ async def on_message(message):
 				print('randomadness stoper')
 				del LP[:]
 				j=-1
-	if message.content.startswith('!commande'):
-		cmd='voici la liste des commandes : \n!randomadness pour lancer le jeu (modo seulement) , \n!join pour vous mettre en liste d attente , \n!liste pour afficher la liste des participants , \n!unjoin pour vous retirer de la liste d attente , \n!duel pour tirer au sort un match , \n!stop pour arreter le jeu (modo seulement) ,\n!commande pour afficher la liste des commandes .'
+	if message.content.startswith('!commandes'):
+		cmd="Voici la liste des commandes que vous pouvez utiliser : \n!randomadness pour lancer le jeu (modo seulement) , \n!join pour vous mettre en liste d'attente , \n!liste pour afficher la liste des participants , \n!unjoin pour vous retirer de la liste d'attente , \n!duel pour tirer au sort un match , \n!stop pour arrêter le jeu (modo seulement) ,\n!commandes pour afficher la liste des commandes ."
 		await client.send_message(discord.Object(id='383618886806929419'),cmd.format())
 
 @client.event
@@ -108,6 +108,6 @@ async def on_ready():
 	print('------')
 
 #client.run('MzgwMTMwMTYyMjI3OTM3Mjgy.DO0Idg.TTH3LMVssf9BC2mncXXOZcXeusc')	#jon test bot
-client.run('MzgyOTc0NDI1MDA5NDg3ODgz.DPdgSQ.owv2T2CX-xHuip0MhDjUI7R6WZQ')	#test bot priver
-#client.run('MzgzNjE5MjIzNzU2MjEwMTc4.DPm45Q.Oy-nKyLUIurYn-wkMZnezBoaeb8')	#santosvallée
+#client.run('MzgyOTc0NDI1MDA5NDg3ODgz.DPdgSQ.owv2T2CX-xHuip0MhDjUI7R6WZQ')	#test bot priver
+client.run('MzgzNjE5MjIzNzU2MjEwMTc4.DPm45Q.Oy-nKyLUIurYn-wkMZnezBoaeb8')	#santosvallée
 #discord.Object(id='383618886806929419')
